eaf15b8f0df4bf2b5698d7b397310c5b1fd738bc
snomiao <7323030+snomiao@users.noreply.github.com>
2025-05-16 06:55:35 +0000

    format: Apply prettier --fix changes

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
 
   useSWR(
     "/filelist",
-    async () => workingDir && (await scanFilelist(workingDir))
+    async () => workingDir && (await scanFilelist(workingDir)),
   );
 
   const monaco = useMonaco();
@@ -56,7 +56,7 @@ export default function Home() {
     if (!monaco || !editor) return;
     editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, async () => {
       const savebtn = window.document.querySelector(
-        "#save-workflow"
+        "#save-workflow",
       ) as HTMLButtonElement;
       savebtn?.click();
     });
@@ -85,7 +85,7 @@ export default function Home() {
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error(
-          `Failed to fetch file from URL: ${response.statusText}`
+          `Failed to fetch file from URL: ${response.statusText}`,
         );
       }
 
@@ -93,7 +93,7 @@ export default function Home() {
       const extension = url.split(".").pop()?.toLowerCase() || "";
 
       const isSupported = ["png", "webp", "flac", "mp4"].some(
-        (ext) => contentType.includes(ext) || extension === ext
+        (ext) => contentType.includes(ext) || extension === ext,
       );
 
       if (!isSupported) {
@@ -112,7 +112,7 @@ export default function Home() {
       toast.error(
         `Error loading file from URL: ${
           error instanceof Error ? error.message : String(error)
-        }`
+        }`,
       );
       console.error("Error loading file from URL:", error);
     }
@@ -132,7 +132,7 @@ export default function Home() {
           await readWorkflowInfo(e).catch((err) => {
             toast.error(`FAIL to read ${e.name}\nCause:${String(err)}`);
             return null;
-          })
+          }),
       )
       .filter()
       .toArray();
@@ -364,8 +364,8 @@ export default function Home() {
                 {!workingDir
                   ? "(download)"
                   : snap.editing_filename === tasklist[snap.editing_index]?.name
-                  ? "(overwrite)"
-                  : "(save as)"}
+                    ? "(overwrite)"
+                    : "(save as)"}
               </span>
             </button>
           </div>
@@ -428,7 +428,7 @@ export default function Home() {
 
   async function writeToWorkingDir(
     workingDir: FileSystemDirectoryHandle,
-    file: File
+    file: File,
   ) {
     const h = await workingDir.getFileHandle(file.name, {
       create: true,
@@ -486,7 +486,7 @@ function tryPrettyJson(json: string) {
 
 function chooseNthFileToEdit(
   tasklist: Awaited<ReturnType<typeof readWorkflowInfo>>[],
-  i: number
+  i: number,
 ) {
   if (!tasklist[i]) {
     persistState.editing_index = -1;
